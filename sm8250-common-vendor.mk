0fe90b36302aa904346f9f8a7d9dff979f9c5603
isot <efeisotkmr@gmail.com>
2025-02-14 16:10:09 +0300

    Conflicting files removed

--- sm8250-common-vendor.mk
+++ sm8250-common-vendor.mk
@@ -208,24 +208,19 @@ PRODUCT_PACKAGES += \
     libadpcmdec \
     libaudcal \
     libaudioparsers \
-    libbatterylistener \
     libbluetooth_audio_session_qti \
     libbluetooth_audio_session_qti_2_1 \
     libcapiv2svacnn \
     libcapiv2svarnn \
     libcapiv2vop \
-    libcomprcapture \
     libdeccfg \
     libdsd2pcm \
-    libexthwplugin \
     libfastcrc \
     libgcs-calwrapper \
     libgcs-ipc \
     libgcs-osal \
     libgcs \
-    libhdmiedid \
     libhdmipassthru \
-    libhfp \
     liblistensoundmodel2 \
     libmm-hdcpmgr \
     libmmosal_vendor \
@@ -240,8 +235,6 @@ PRODUCT_PACKAGES += \
     libqcodec2_v4l2codec \
     libqtigef \
     libsmwrapper \
-    libsndmonitor \
-    libspkrprot \
     libssrec \
     libstagefright_soft_ac4dec \
     libstagefright_soft_ddpdec \
